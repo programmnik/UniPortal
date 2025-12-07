731e1d7dc848571a78cf80aa2df0124c649ccd0d
programmnik <nikitasmatko184@gmail.com>
2025-12-07 21:22:17 +0300

    улучшение

--- scripts/per-main.js
+++ scripts/per-main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+window.addEventListener('load', () => {
     const user = getCurrentUser();
     if(user){
         const profileNameElements = document.querySelectorAll('.user-profile-name');
