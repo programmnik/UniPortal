b45179e6ccc3357dd06843728c0e1ac0ce6fe385
programmnik <nikitasmatko184@gmail.com>
2026-01-01 11:42:00 +0300

    corrent project

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
 app.get('/', (req, res) => {
     res.render('index');
